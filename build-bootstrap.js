c058be926a31d320bd98af832e6c9017e4fbc5ac
Dominik Broj <19861998+thetric@users.noreply.github.com>
2019-04-14 21:10:38 +0200

    build(bootstrap): minify css

--- build-bootstrap.js
+++ build-bootstrap.js
@@ -5,6 +5,7 @@ const postcssPresetEnv = require("postcss-preset-env");
 const { writeFile } = require("fs");
 const { render: renderScss } = require("node-sass");
 const postcss = require("postcss");
+const CleanCSS = require("clean-css");
 
 const inFile = "keepassxc-browser/styles/bootstrap.scss";
 const outFile = "keepassxc-browser/styles/bootstrap.css";
@@ -19,11 +20,10 @@ function buildBootstrap() {
             .process(result.css, { from: undefined, to: outFile })
             .then(
                 result => {
-                    writeFile(outFile, result.css, () => true);
+                    const minifiedCssResult = new CleanCSS({level: 2}).minify(result.content);
+                    writeFile(outFile, minifiedCssResult.styles, () => true);
                 },
-                e => {
-                    console.error("postcss processing failed:", e);
-                }
+                e => console.error("postcss processing failed:", e)
             );
     });
 }
