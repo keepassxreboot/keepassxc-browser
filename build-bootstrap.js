1163054a9047b0aadee8981c20d9ff99916cb66c
Dominik Broj <19861998+thetric@users.noreply.github.com>
2019-06-02 18:14:25 +0200

    build(bootstrap): replace node-sass with sass (dart-sass)

--- build-bootstrap.js
+++ build-bootstrap.js
@@ -3,7 +3,7 @@
 const autoprefixer = require("autoprefixer");
 const postcssPresetEnv = require("postcss-preset-env");
 const { writeFile } = require("fs");
-const { render: renderScss } = require("node-sass");
+const { renderSync: renderScss } = require("sass");
 const postcss = require("postcss");
 const CleanCSS = require("clean-css");
 
@@ -11,24 +11,22 @@ const inFile = "keepassxc-browser/styles/bootstrap.scss";
 const outFile = "keepassxc-browser/styles/bootstrap.css";
 
 function buildBootstrap() {
-    renderScss({ file: inFile }, (err, result) => {
-        if (err) {
-            console.error("SCSS build failed:", err);
-            return;
-        }
-        postcss([postcssPresetEnv, autoprefixer])
-            .process(result.css, { from: undefined, to: outFile })
-            .then(
-                result => {
-                    const minifiedCssResult = new CleanCSS({level: 2}).minify(result.content);
-                    writeFile(outFile, minifiedCssResult.styles, () => true);
-                },
-                e => console.error("postcss processing failed:", e)
-            );
-    });
+    const result = renderScss({ file: inFile });
+    postcss([postcssPresetEnv, autoprefixer])
+        .process(result.css, { from: undefined, to: outFile })
+        .then(
+            result => {
+                const minifiedCssResult = new CleanCSS({ level: 2 }).minify(
+                    result.content
+                );
+                writeFile(outFile, minifiedCssResult.styles, () => true);
+            },
+            e => console.error("postcss processing failed:", e)
+        );
 }
 exports.buildBootstrap = buildBootstrap;
 
 if (require.main === module) {
     buildBootstrap();
 }
+
