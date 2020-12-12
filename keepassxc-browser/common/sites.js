b68453fa893c87a93589af32d82765cc2538b023
varjolintu <sami.vanttinen@protonmail.com>
2020-12-12 10:03:09 +0200

    Add TOTP possibility to Predefined Sites

--- keepassxc-browser/common/sites.js
+++ keepassxc-browser/common/sites.js
@@ -66,3 +66,12 @@ kpxcSites.exceptionFound = function(classList) {
 
     return false;
 };
+
+kpxcSites.expectedTOTPMaxLength = function() {
+    if (document.location.origin.startsWith('https://www.amazon')
+        && document.location.href.includes('/ap/mfa')) {
+        return 20;
+    }
+
+    return MAX_TOTP_INPUT_LENGTH;
+};
