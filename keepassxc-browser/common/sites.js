9059a78e2f52d78e73a898537a4a53d6b6ae471d
varjolintu <sami.vanttinen@protonmail.com>
2021-01-09 11:55:33 +0200

    Fix Auto-Submit for eBay

--- keepassxc-browser/common/sites.js
+++ keepassxc-browser/common/sites.js
@@ -41,6 +41,7 @@ const PREDEFINED_SITELIST = [
 ];
 
 const kpxcSites = {};
+kpxcSites.ebayUrl = 'https://www.ebay.';
 kpxcSites.googlePasswordFormUrl = 'https://accounts.google.com/signin/v2/challenge/password';
 kpxcSites.googleUrl = 'https://accounts.google.com';
 kpxcSites.savedForm = undefined;
