a33cde9871da981c583bfd53406d6de1b38b358e
varjolintu <sami.vanttinen@ahmala.org>
2024-06-09 13:22:16 +0300

    Rename functions to prevent override

--- keepassxc-browser/content/passkeys.js
+++ keepassxc-browser/content/passkeys.js
@@ -15,22 +15,22 @@ const PASSKEYS_UNKNOWN_ERROR = 31;
 const PASSKEYS_INVALID_CHALLENGE = 32;
 const PASSKEYS_INVALID_USER_ID = 33;
 
-const stringToArrayBuffer = function(str) {
+const kpxcStringToArrayBuffer = function(str) {
     const arr = Uint8Array.from(str, c => c.charCodeAt(0));
     return arr.buffer;
 };
 
 // From URL encoded base64 string to ArrayBuffer
-const base64ToArrayBuffer = function(str) {
-    return stringToArrayBuffer(window.atob(str?.replaceAll('-', '+').replaceAll('_', '/')));
+const kpxcBase64ToArrayBuffer = function(str) {
+    return kpxcStringToArrayBuffer(window.atob(str?.replaceAll('-', '+').replaceAll('_', '/')));
 };
 
 // Wraps response to AuthenticatorAttestationResponse object
 const createAttestationResponse = function(publicKey) {
     const response = {
-        attestationObject: base64ToArrayBuffer(publicKey.response.attestationObject),
-        clientDataJSON: base64ToArrayBuffer(publicKey.response.clientDataJSON),
-        getAuthenticatorData: () => base64ToArrayBuffer(publicKey.response?.authenticatorData),
+        attestationObject: kpxcBase64ToArrayBuffer(publicKey.response.attestationObject),
+        clientDataJSON: kpxcBase64ToArrayBuffer(publicKey.response.clientDataJSON),
+        getAuthenticatorData: () => kpxcBase64ToArrayBuffer(publicKey.response?.authenticatorData),
         getTransports: () => [ 'internal' ]
     };
 
@@ -40,10 +40,10 @@ const createAttestationResponse = function(publicKey) {
 // Wraps response to AuthenticatorAssertionResponse object
 const createAssertionResponse = function(publicKey) {
     const response = {
-        authenticatorData: base64ToArrayBuffer(publicKey.response?.authenticatorData),
-        clientDataJSON: base64ToArrayBuffer(publicKey.response?.clientDataJSON),
-        signature: base64ToArrayBuffer(publicKey.response?.signature),
-        userHandle: publicKey.response?.userHandle ? base64ToArrayBuffer(publicKey.response?.userHandle) : null
+        authenticatorData: kpxcBase64ToArrayBuffer(publicKey.response?.authenticatorData),
+        clientDataJSON: kpxcBase64ToArrayBuffer(publicKey.response?.clientDataJSON),
+        signature: kpxcBase64ToArrayBuffer(publicKey.response?.signature),
+        userHandle: publicKey.response?.userHandle ? kpxcBase64ToArrayBuffer(publicKey.response?.userHandle) : null
     };
 
     return Object.setPrototypeOf(response, AuthenticatorAssertionResponse.prototype);
@@ -57,7 +57,7 @@ const createPublicKeyCredential = function(publicKey) {
     const publicKeyCredential = {
         authenticatorAttachment: publicKey.authenticatorAttachment,
         id: publicKey.id,
-        rawId: base64ToArrayBuffer(publicKey.id),
+        rawId: kpxcBase64ToArrayBuffer(publicKey.id),
         response: authenticatorResponse,
         type: publicKey.type,
         clientExtensionResults: () => publicKey?.response?.clientExtensionResults || {},
