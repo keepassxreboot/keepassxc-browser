efba157791dc127482642bfd868efe904cc4eb15
Eamon Nerbonne <eamon@nerbonne.org>
2025-09-28 11:59:49 +0200

    Since promo is a prefix of promotional, this isn't even necessary! :-)

--- keepassxc-browser/content/totp-field.js
+++ keepassxc-browser/content/totp-field.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const ignoreRegex = /(bank|coupon|postal|user|zip|promo|promotional).*code|(en|de)code(d|r)*|comment|author|error/i;
+const ignoreRegex = /(bank|coupon|postal|user|zip|promo).*code|(en|de)code(d|r)*|comment|author|error/i;
 const ignoredTypes = [ 'email', 'password', 'username' ];
 const allowedInputTypes = [ 'number', 'password', 'tel', 'text' ];
 
